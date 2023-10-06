f972ab5c0ccfbbf00385c68d39148f18aa597eef
devxb <dlwnsdud205@naver.com>
2023-10-06 19:09:48 +0900

    feat: CreateRoomController를 구현한다

---
+++ word/src/main/kotlin/xb/dev/word/controller/CreateRoomController.kt
@@ -0,0 +1,21 @@
+package xb.dev.word.controller
+
+import org.springframework.stereotype.Component
+import xb.dev.engine.dispatcher.Handleable
+import xb.dev.engine.server.Caffeine
+import xb.dev.word.service.CreateRoomService
+
+@Component
+internal class CreateRoomController(private val createRoomService: CreateRoomService) : Handleable {
+
+    private val name = "createRoom"
+
+    override fun isHandleable(methodName: String): Boolean = name == methodName
+
+    override fun handle(caffeine: Caffeine): String {
+        val ownerId = caffeine.parameters["ownerId"]?.toLong()
+            ?: throw IllegalArgumentException("Missing parameter \"ownerId\"")
+
+        createRoomService.create(ownerId)
+    }
+}
