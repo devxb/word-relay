72118d4a2df5bdfe7f197d4885b68f69ccac8685
devxb <dlwnsdud205@naver.com>
2023-10-17 16:37:38 +0900

    feat: CreateRoomHandler를 구현한다

---
+++ client/src/main/kotlin/xb/dev/client/controller/CreateRoomHandler.kt
@@ -0,0 +1,20 @@
+package xb.dev.client.controller
+
+import org.springframework.stereotype.Service
+import xb.dev.client.domain.Handler
+import xb.dev.client.domain.SupportMessage
+import xb.dev.client.domain.WordMessenger
+
+@Service
+internal class CreateRoomHandler(private val wordMessenger: WordMessenger): Handler {
+
+    override fun isHandleable(name: String): Boolean = name == "createRoom"
+
+    override fun handle(arg: Map<String, String>) {
+        val ownerId = arg["ownerId"]?.toInt()
+            ?: throw IllegalArgumentException("Missing parameter \"ownerId\"")
+        val token = arg["token"] ?: throw IllegalArgumentException("Missing parameter \"token\"")
+
+        wordMessenger.send(SupportMessage.CreateRoom(ownerId, token))
+    }
+}
